9473a65c0e710113e4c24b4e5ed1e7ece48a1916
Ananya200 <75524064+Ananya200@users.noreply.github.com>
2021-05-03 17:15:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,62 @@
-var snow , snowbg_img,snow_bg;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var snowbg_img,snow_bg;
 var girl,girl_img;
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=5;
+
+var snowCreatedFrame=0;
+
 function preload(){
-   snowbg_img = loadImage("snow2.jpg");
-   girl_img = loadAnimation("girl walking.gif");
+
+   girl_img = loadImage("girl walking.gif");
+
 }
 function setup() {
+
+  engine = Engine.create();
+  world = engine.world;
+
   createCanvas(1280,610);
      
-  snow_bg = createSprite(640,250,50,50);
-  snow_bg.addImage(snowbg_img);
-  snow_bg.scale = 1;
-
   girl = createSprite(1200,450,50,50);
   girl . addAnimation("girlanimation",girl_img);
- // girl . addAnimation("girl1animation",girl_img);
+  //girl . addVideo("girl1animation",girl_img);
   girl.scale = 0.4;
+
+  snowbg_img = loadImage("snow1.jpg");
+
+  if(frameCount % 5000 === 0){
+
+    for(var i=0; i<maxDrops; i++){
+      drops.push(new snowflake(random(0,1280), random(0,610)));
+    }
+
+  }
+
 }
 
 function draw() {
-  background(0,0,0);
+  Engine.update(engine);
+  background(snowbg_img);
+
+  textFont("Bradley Hand ITC");
+  textSize(27);
+  textStyle(BOLD);
+  fill(64,124,193);
+  text("Snowfall is beautiful!",520,50)
+
+  for(var i = 0; i<maxDrops; i++){
+    drops[i].showDrop();
+    drops[i].updateY();
+ }
 
   drawSprites();
+
 }
--- snowfall.js
+++ snowfall.js
@@ -1,22 +1,25 @@
-class snowfall {
-    constructor(x,y){
-      var option={
-        isStatic:false
+class snowflake {
+  constructor(x,y){
+      var options = {
+          friction: 0.001,
+          restitution:0.1           
       }
-      this.image = loadImage("snow4.webp")
-      this.snow=Bodies.circle(x,y,20,option);
-      this.radius=20;
-      World.add(world,this.snow);
-    }
-    update(){
-        if(this.snow.position.y>height){
-            Matter.Body.setPosition(this.snow,{x:random(0,600),y:random(0,100)});
-        }
-    }
-    display() {
-      var pos=this.snow.position;
-      imageMode(CENTER);
-      image(this.image,pos.x,pos.y,this.radius,this.radius);
-    }
+      this.rain = Bodies.circle(x,y,5,options)
+      this.radius = 0;
+      this.img = loadImage("snow4.webp");
+      World.add(world, this.rain);
   }
+
+  updateY(){     
+      if(this.rain.position.y > height){
+
+          Matter.Body.setPosition(this.rain, {x:random(0,1280), y:random(0,0)})
+      }
+  }
+
+  showDrop(){
+      imageMode(CENTER);
+      image(this.img,this.rain.position.x,this.rain.position.y,this.radius+20,this.radius+20);
+  }
+}
   
